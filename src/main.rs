4e4941f2c2d3b671a606202e8a05ee181443204b
luckychacha <358140284@qq.com>
2023-12-04 10:23:29 +0800

    Move type implements from main.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
-use hmac::Hmac;
-use pbkdf2::pbkdf2;
-use sha2::Sha512;
-use std::fmt::Write;
+use crate::mnemonic_seed::MnemonicSeed;
 
-struct MnemonicSeed {
-    mnemonic: String,
-    password: Option<String>,
-}
-
-// Number of iterations
-const PBKDF2_ROUNDS: u32 = 2048;
-
-// 512-bit seed
-const PBKDF2_BYTES: usize = 64;
-
-impl MnemonicSeed {
-    fn new(mnemonic: String, password: Option<String>) -> Self {
-        MnemonicSeed { mnemonic, password }
-    }
-
-    fn generate_seed(&self) -> [u8; PBKDF2_BYTES] {
-        let salt = format!("mnemonic{}", self.password.clone().unwrap_or_default());
-        let mut seed = [0u8; PBKDF2_BYTES];
-
-        pbkdf2::<Hmac<Sha512>>(
-            self.mnemonic.as_bytes(),
-            salt.as_bytes(),
-            PBKDF2_ROUNDS,
-            &mut seed,
-        )
-            .expect("HMAC can be initialized with any key length");
-        seed
-    }
-
-}
+mod mnemonic_seed;
 
 fn main() {
-    let mnemonic = "example mnemonic phrase here".to_string();
-    let password = Some("optional password".to_string());
+    let mnemonic =
+        "letter advice cage absurd amount doctor acoustic avoid letter advice cage above"
+            .to_string();
+    let password = Some("TREZOR".to_string());
     let mnemonic_seed = MnemonicSeed::new(mnemonic, password);
 
     let seed = mnemonic_seed.generate_seed();
-    println!("Seed: {:?}", hex::encode(seed));
+    assert_eq!(
+        "d71de856f81a8acc65e6fc851a38d4d7ec216fd0796d0a6827a3ad6ed5511a30fa280f12eb2e47ed2ac03b5c462a0358d18d69fe4f985ec81778c1b370b652a8",
+        hex::encode(seed)
+    );
 }
---
+++ src/mnemonic_seed.rs
@@ -0,0 +1,61 @@
+use hmac::digest::{Digest, KeyInit};
+use hmac::{
+    digest::{
+        block_buffer::Eager,
+        core_api::{BlockSizeUser, BufferKindUser, CoreProxy, FixedOutputCore, UpdateCore},
+        typenum::{IsLess, Le, NonZero, U256},
+        HashMarker, Mac,
+    },
+    Hmac,
+};
+use pbkdf2::pbkdf2;
+use sha2::Sha256;
+use sha2::Sha512;
+
+pub struct MnemonicSeed {
+    mnemonic: String,
+    password: Option<String>,
+}
+
+// Number of iterations
+const PBKDF2_ROUNDS: u32 = 2048;
+
+// 512-bit seed
+const PBKDF2_BYTES: usize = 64;
+
+impl MnemonicSeed {
+    pub fn new(mnemonic: String, password: Option<String>) -> Self {
+        MnemonicSeed { mnemonic, password }
+    }
+
+    pub fn generate_seed(&self) -> [u8; PBKDF2_BYTES] {
+        let salt = format!("mnemonic{}", self.password.clone().unwrap_or_default());
+        let mut seed = [0u8; PBKDF2_BYTES];
+
+        pbkdf2::<Hmac<Sha512>>(
+            self.mnemonic.as_bytes(),
+            salt.as_bytes(),
+            PBKDF2_ROUNDS,
+            &mut seed,
+        )
+        .expect("HMAC can be initialized with any key length");
+        seed
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_seed() {
+        let mnemonic =
+            "legal winner thank year wave sausage worth useful legal winner thank yellow"
+                .to_string();
+        let password = Some("TREZOR".to_string());
+        let mnemonic_seed = MnemonicSeed::new(mnemonic, password);
+
+        let seed = mnemonic_seed.generate_seed();
+        assert_eq!(hex::encode(seed), "2e8905819b8723fe2c1d161860e5ee1830318dbf49a83bd451cfb8440c28bd6fa457fe1296106559a3c80937a1c1069be3a3a5bd381ee6260e8d9739fce1f607")
+    }
+}
